ca6fb0534e28f27cee309cd8a0a705b4e64abb59
ahasani <albin.hasani@ikubinfo.al>
2018-06-11 15:45:46 +0200

    Add strings shell script

---
+++ Strings/strings.sh
@@ -0,0 +1,11 @@
+set inventory:4x100m-womens-final 1000
+get inventory:4x100m-womens-final
+decrby inventory:4x100m-womens-final 1
+get inventory:4x100m-womens-final
+type inventory:4x100m-womens-final
+object encoding inventory:4x100m-womens-final
+set inventory:4x100m-womens-final "Sold Out"
+type inventory:4x100m-womens-final
+object encoding inventory:4x100m-womens-final
+decrby inventory:4x100m-womens-final 1
+set inventory:4x100m-womens-final 0
